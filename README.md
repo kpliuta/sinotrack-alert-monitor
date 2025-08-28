edd7225bde09479da804f6217b827994515e0571
kpliuta <9212935+kpliuta@users.noreply.github.com>
2025-08-28 10:38:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# sinotrack-alert-monitor
+
+## How to run
+- may be executed using termux-web-scraper framework in a termux environment on android device
+- or alternatively as a python selenium script in a desktop environment
---
+++ termux-runner.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Check if git is installed
+if ! command -v git >/dev/null 2>&1; then
+    echo "Error: git is not installed. Please install it and try again."
+    exit 1
+fi
+
+# Get the absolute path to the scenario directory.
+WORK_DIR=$(realpath "$(dirname "$0")")
+
+# Clone or update the termux-web-scraper repository.
+if [ -d "$WORK_DIR/termux-web-scraper" ]; then
+  echo "Updating termux-web-scraper..."
+  git -C "$WORK_DIR/termux-web-scraper" pull --rebase
+else
+  echo "Cloning termux-web-scraper..."
+  # TODO: specify a version of termux-web-scraper
+  git clone https://github.com/kpliuta/termux-web-scraper.git "$WORK_DIR/termux-web-scraper"
+fi
+
+# TODO: change a timeout to once in 10 min
+# Execute the scraper script in a loop with a 10 seconds timeout.
+"$WORK_DIR/termux-web-scraper/scripts/run.sh" \
+    --work-dir "$WORK_DIR" \
+    --scenario-file src/main.py \
+    --upgrade \
+    --loop \
+    --loop-timeout 10 \
+    --loop-error-ignore \
+    --output-dir /sdcard/sinotrack-alert-monitor:/mnt/scraper/out
